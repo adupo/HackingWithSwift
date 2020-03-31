9233d8f2279518867e366b0de18d58372a318792
Aaron Dupont <aarondupont@Chevron.com>
2020-03-31 16:24:19 -0500

    HWS Day 21 Review and Proj 2 challenge

--- UiKit/Project2/Project2/ViewController.swift
+++ UiKit/Project2/Project2/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var round = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,27 +35,43 @@ class ViewController: UIViewController {
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        round += 1
         
         Button1.setImage(UIImage(named: countries[0]), for: .normal)
         Button2.setImage(UIImage(named: countries[1]), for: .normal)
         Button3.setImage(UIImage(named: countries[2]), for: .normal)
         
-        title = countries[correctAnswer].uppercased()
+        title = "Guess: " + countries[correctAnswer].uppercased() + " - Score: \(score)"
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        var additionalMessage = ""
         
         if sender.tag == correctAnswer {
             title = "Corrrect!"
             score += 1
         } else {
             title = "Wrongo :("
+            additionalMessage = "BTW, thats the flag of \(countries[sender.tag].uppercased())"
             score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is now \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        let ac = UIAlertController(title: title, message: "Your score is \(score). "+additionalMessage, preferredStyle: .alert)
+        
+        if (round == 10) {
+            if (score > 5) {
+                title = "You passed!"
+            } else {
+                title = "This is troubling..."
+            }
+            score = 0
+            round = 0
+            ac.title = title
+            ac.addAction(UIAlertAction(title: "Start Over", style: .destructive, handler: askQuestion))        }
+        else {
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        }
         present(ac, animated: true)
     }
     
