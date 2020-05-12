f4db77ef11730187f50e71fda762213c6efa3d85
Aaron Dupont <17016950+adupo@users.noreply.github.com>
2020-05-12 08:57:14 -0500

    Day 24 done

---
+++ UiKit/Project4/Project4/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Aaron Dupont on 5/12/20.
+//  Copyright © 2020 AaronDupont. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "netflix.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "twitter.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://"+actionTitle) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
+
+}
+
