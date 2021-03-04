b42712e3f3a9774088addb8eba6bd7b1f8b8638f
adupo <17016950+adupo@users.noreply.github.com>
2021-03-04 06:40:51 -0500

    day 42; project 10 part 1

--- UiKit/Consolidation4/Consolidation4/ViewController.swift
+++ UiKit/Consolidation4/Consolidation4/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var correctWord: UILabel!
+    
+    let word = "technical"
+    var usedLetters = ""
+    var guessedWord = "" {
+        didSet {
+            title = guessedWord
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(guessLetterPrompt))
+        
+        guessedWord = String.init(repeating: "?", count: word.count)
+        
+        
     }
+    
+    func guessFor(_ letter: Character) {
+        for letter in word {
+            if usedLetters.contains(letter) {
+                guessedWord.append(String(letter))
+            } else {
+                guessedWord.append("?")
+            }
+        }
+    }
+    
+    @objc func guessLetterPrompt() {
+        let ac = UIAlertController(title: "Guess one letter", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submit = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let letter = ac?.textFields?[0].text else { return }
+            self?.guessFor(letter)
+        }
+        ac.addAction(submit)
+        present(ac, animated: true)
+      }
 
 
 }
---
+++ UiKit/Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Aaron Dupont on 3/4/21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var ImageView: UIImageView!
+    @IBOutlet var Name: UILabel!
+    
+}
