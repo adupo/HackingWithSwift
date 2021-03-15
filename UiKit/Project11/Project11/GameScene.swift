82a401d2e61c72b55be180ac675df6cdb214ddb3
adupo <17016950+adupo@users.noreply.github.com>
2021-03-15 07:16:38 -0400

    day 47 - SpriteKit challenge

--- UiKit/Project11/Project11/GameScene.swift
+++ UiKit/Project11/Project11/GameScene.swift
@@ -26,6 +26,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     
+    var ballCount = 0
+    
+    let ballColors = ["Blue", "Cyan", "Green", "Grey", "Purple", "Red", "Yellow"]
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
@@ -73,24 +77,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 // create random boxes where user is tapping
                 let size = CGSize(width: Int.random(in: 16...128), height: 16)
                 let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                box.name = "box"
                 box.zRotation = CGFloat.random(in: 0...3)
                 box.position = location
                 box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
                 box.physicsBody?.isDynamic = false
                 addChild(box)
             } else {
-                //add balls to screen ( user is playing )
-                let ball = SKSpriteNode(imageNamed: "ballRed")
-                
-                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-                ball.physicsBody?.restitution = 0.4
-                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-                ball.position = location
-                ball.name = "ball"
-                
-                addChild(ball)
+                if ballCount < 5 {
+                    //add balls to screen ( user is playing )
+                    let ballImageName = "ball\(ballColors.randomElement() ?? "Red")"
+                    let ball = SKSpriteNode(imageNamed: ballImageName)
+                    ballCount += 1
+                    
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                    ball.physicsBody?.restitution = 0.4
+                    ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                    ball.position.x = location.x
+                    ball.position.y = 650
+                    ball.name = "ball"
+                    
+                    addChild(ball)
+                } else {
+                    resetGame()
+                }
             }
         }
+    }
+    
+    //this doesnt work great.. sometimes uses outdated score if user spam clicks. Why?
+    func resetGame() {
+        let ac = UIAlertController(title: "Game Over", message: "Your score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Rest Game", style: .default) { [weak self] _ in
+            self?.score = 0
+            self?.ballCount = 0
+        })
+        self.view?.window?.rootViewController?.present(ac, animated: true)
     }
     
     func makeBouncer(at position: CGPoint) {
@@ -133,13 +155,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if object.name == "good" {
             destroy(ball: ball)
             score += 1
+            ballCount -= 1 //give the user an extra ball
         } else if object.name == "bad" {
             destroy(ball: ball)
             score -= 1
+        } else if object.name == "box" {
+            object.removeFromParent()
         }
     }
     
     func destroy(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
         ball.removeFromParent()
     }
     
